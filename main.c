ae2a9f4bb4b0bc2bda843c3bf00c281890ebe1dc
Dragan Mihai-Alexandru <113632641+mihoi1@users.noreply.github.com>
2026-03-28 16:04:14 +0200

    Final version

--- main.c
+++ main.c
@@ -1,57 +1,54 @@
+#include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define DDRD (*(volatile unsigned char*)0x2A)
-#define PORTD (*(volatile unsigned char*)0x2B)
-#define PIND (*(volatile unsigned char*)0x29)
-
-#define LED_GREEN 6
-#define LED_RED 7
-#define ALARM_PIN 2
-#define SENSOR_1 4
-#define SENSOR_2 5
+#define LED_GREEN     PD6
+#define LED_RED       PD7
+#define PANIC_BUTTON  PD2   
+#define BUTTON_1      PD4
+#define BUTTON_2      PD5
+#define ALARM_DURATION_MS 5000  
 
 volatile uint8_t activeAlarm = 0;
 
-void setup() {
-  cli();
-
-  DDRD |= (1 << LED_GREEN) | (1 << LED_RED);
-
-  DDRD &= ~((1 << ALARM_PIN) | (1 << SENSOR_1) | (1 << SENSOR_2));
-
-  PORTD |= (1 << ALARM_PIN) | (1 << SENSOR_1) | (1 << SENSOR_2);
-
-  EICRA |= (1 << ISC01);
-  EICRA &= ~(1 << ISC00);
-
-  EIMSK |= (1 << INT0);
-
-  sei();
-}
-
 ISR(INT0_vect) {
-  activeAlarm = 1;
+    activeAlarm = 1;
 }
 
-void loop() {
-  if (activeAlarm == 1) {
-    PORTD ^= (1 << LED_RED);
-    PORTD &= ~(1 << LED_GREEN);
-    _delay_ms(500);
-    return;
-  }
+int main(void) {
+    DDRD |= (1 << LED_GREEN) | (1 << LED_RED);
+    DDRD &= ~((1 << PANIC_BUTTON) | (1 << BUTTON_1) | (1 << BUTTON_2));
+    PORTD |= (1 << PANIC_BUTTON) | (1 << BUTTON_1) | (1 << BUTTON_2);
 
-  uint8_t state = PIND & ((1 << SENSOR_1) | (1 << SENSOR_2));
+    EICRA |= (1 << ISC01);
+    EIMSK |= (1 << INT0);
+    
+    sei(); 
 
-  if (state == 0) {
-    PORTD |= (1 << LED_GREEN);
-    PORTD &= ~(1 << LED_RED);
-  } 
-  else {
-    PORTD &= ~(1 << LED_GREEN);
-    PORTD |= (1 << LED_RED);
-  }
+    uint16_t elapsed = 0;
 
-  _delay_ms(50);
+    while (1) {
+        if (activeAlarm) {
+            PORTD ^= (1 << LED_RED);
+            PORTD &= ~(1 << LED_GREEN);
+            _delay_ms(500);
+            elapsed += 500;
+
+            if (elapsed >= ALARM_DURATION_MS) {
+                activeAlarm = 0;
+                elapsed = 0;
+                PORTD &= ~(1 << LED_RED);
+            }
+        } else {
+            // Verificare simultaneitate butoane
+            if (!(PIND & (1 << BUTTON_1)) && !(PIND & (1 << BUTTON_2))) {
+                PORTD |= (1 << LED_GREEN);
+                PORTD &= ~(1 << LED_RED);
+            } else {
+                PORTD |= (1 << LED_RED);
+                PORTD &= ~(1 << LED_GREEN);
+            }
+            _delay_ms(50);
+        }
+    }
 }
